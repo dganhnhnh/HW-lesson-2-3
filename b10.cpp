ef1e29782dba7bbaf5c447a51270afdaaf0857ac
duonganh <duonganh2973@gmail.com>
2021-07-22 14:31:51 +0700

    add b10.cpp file

--- b10.cpp
+++ b10.cpp
@@ -3,17 +3,32 @@
 #include <string>
 using namespace std;
 
-int n;
+int n,minBase; bool hey;
 
-bool isSumOfExponentials(int n){
-    int h=(n%2==0)? (n/2):((n-1)/2);
-    for(int i=2; i<=h;i++){
-        while(n>0){
-            
+void isSumOfExponentials(int n){
+    int h=(n%2==0)? (n/2):((n-1)/2);  
+    for(int i=3; i<=h;i++){
+        int temp =n; int cnt=0;
+        if(n %i==0){
+            int upperHat = int(log(n) / log(i));
+            for(int j=upperHat; j>=1; j--){
+                if (pow(i, j) <= temp){
+                        temp -= pow(i, j); 
+                        cnt++;
+                }
+            }
+              
         }
+        if(cnt >= 2 && temp == 0){
+                minBase = i; return;
+            } 
     }
+    minBase=-1; return;
 }
 
 int main(){
     cin>>n;
+    isSumOfExponentials(n);
+    cout <<minBase; 
+    return 0;
 }
